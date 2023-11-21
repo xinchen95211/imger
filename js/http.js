1489a0f0b9bd76cb0abe9c7ff1267cf86d67d8dd
xinchen95211 <76147759+xinchen95211@users.noreply.github.com>
2023-11-21 20:14:03 +0800

    Update http.js

--- js/http.js
+++ js/http.js
@@ -4,7 +4,7 @@ const register_support = (params) => axios.post("/api/register",params);
 //photo封装
 
 //video封装
-const play_video_to_id = (params) => axios.get("https://vernelproxy.dynv6.net/proxy/frp-oak.top:14850/video/" + params);
+const play_video_to_id = (params) => axios.get("https://vernelproxy.dynv6.net/proxy/frp-hat.top:49728/video/" + params);
 
 
 const switchLikes = (params) => axios.get("https://vernelproxy.dynv6.net/proxy/frp-oak.top:14850/video/likes/" + params);
